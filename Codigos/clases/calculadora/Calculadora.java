d300ae7822c92b9195f3cc97802e766f92448f60
Darlex5027 <reseal50@gmail.com>
2023-10-20 17:19:01 -0600

    nuevo puto problema resulto

--- Codigos/clases/calculadora/Calculadora.java
+++ Codigos/clases/calculadora/Calculadora.java
@@ -1,8 +1,44 @@
+import java.util.Scanner;
 public class Calculadora{
+	Scanner entrada=new Scanner(System.in);
 	float n1=0;
 	float n2=0;
 	float n3=0;
 
+	public void menu(){
+		int i=0;
+		do{
+			System.out.println("1)Sumar\n");
+			System.out.println("2)Restar\n");
+			System.out.println("3)Dividir\n");
+			System.out.println("4)Multiplicar\n");
+			System.out.println("5)Potenciaciòn\n");
+			System.out.println("6)Salir\n");	
+			
+			i=entrada.nextInt();
+			if(i!=6)
+				leer();
+			if(i==1)
+				suma();
+			if(i==2)
+				resta();
+			if(i==3)
+				dividir();
+			if(i==4)
+				multiplicar();
+			if(i==5)
+				potenciacion();
+
+		}while(i!=6);
+		
+		
+	}
+	public void leer(){
+		System.out.println("Digita el nùmero 1:");
+		this.n1=entrada.nextFloat();
+		System.out.println("Digita el nùmero 2:");
+		this.n2=entrada.nextFloat();
+	}
 	public void suma(){
 		n3=n1+n2;
 		mostrarResultado('+',n3);
@@ -12,8 +48,14 @@ public class Calculadora{
 		mostrarResultado('-',n3);
 	}
 	public void dividir(){
-		n3=n1/n2;
-		mostrarResultado('/',n3);
+		if(n2==0){
+			System.out.println("No se puede dividir entre 0, intente de nuevo");
+			leer();
+			dividir();
+		}else{
+			n3=n1/n2;
+			mostrarResultado('/',n3);
+		}
 	}
 	public void multiplicar(){
 		n3=n1*n2;
